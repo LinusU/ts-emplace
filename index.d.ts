dc8ea70046a466426b5b7d62748e89629aff233a
Linus Unnebäck <linus@folkdatorn.se>
2023-11-27 13:53:42 +0100

    🐛 Fix compatibility with ES2023 typings

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,12 @@
+// ref: https://github.com/microsoft/TypeScript/pull/54195
+interface WeakKeyTypes {
+  object: object
+}
+
 interface Map<K, V> {
   emplace (key: K, op: { insert?: (key: K, map: Map<K, V>) => V, update?: (existing: V, key: K, map: Map<K, V>) => V }): V
 }
 
-interface WeakMap<K extends object, V> {
+interface WeakMap<K extends WeakKeyTypes[keyof WeakKeyTypes], V> {
   emplace (key: K, op: { insert?: (key: K, map: WeakMap<K, V>) => V, update?: (existing: V, key: K, map: WeakMap<K, V>) => V }): V
 }
